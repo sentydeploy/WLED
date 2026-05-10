923ec22a5c5b8cff8dfb12f4c617700a6c781719
sentyAli <sentydeploy@gmail.com>
2026-05-10 23:49:27 +0300

    Fix WoL usermod: match topic suffix that WLED passes after stripping device prefix

--- usermods/WakeOnLan/usermod_wol.cpp
+++ usermods/WakeOnLan/usermod_wol.cpp
@@ -3,10 +3,20 @@
 
 class WakeOnLanUsermod : public Usermod {
 private:
-  char _mac[18]   = "D8:BB:C1:0C:A2:84";
-  char _topic[48] = "wled/01896c/wol";
-  bool _macParsed = false;
+  char _mac[18]      = "D8:BB:C1:0C:A2:84";
+  char _topic[48]    = "wled/01896c/wol";
+  char _topicSfx[24] = "/wol";
+  bool _macParsed    = false;
   uint8_t _macBytes[6];
+
+  void deriveTopicSuffix() {
+    size_t pfx = strlen(mqttDeviceTopic);
+    if (pfx > 0 && strncmp(_topic, mqttDeviceTopic, pfx) == 0) {
+      strlcpy(_topicSfx, _topic + pfx, sizeof(_topicSfx));
+    } else {
+      strlcpy(_topicSfx, _topic, sizeof(_topicSfx));
+    }
+  }
 
   void parseMac() {
     if (_macParsed) return;
@@ -30,16 +40,24 @@ private:
     udp.beginPacket(IPAddress(255,255,255,255),9);
     udp.write(packet,sizeof(packet));
     udp.endPacket();
+    IPAddress subnetBcast = WiFi.localIP();
+    IPAddress mask = WiFi.subnetMask();
+    for (int i=0;i<4;i++) subnetBcast[i] = subnetBcast[i] | (~mask[i]);
+    udp.beginPacket(subnetBcast,9);
+    udp.write(packet,sizeof(packet));
+    udp.endPacket();
     udp.stop();
     DEBUG_PRINTLN(F("WoL: Magic packet sent!"));
   }
 
 public:
-  void setup() override {}
+  void setup() override {
+    deriveTopicSuffix();
+  }
   void loop() override {}
 
   bool onMqttMessage(char* topic, char* payload) override {
-    if (strcmp(topic, _topic) == 0) {
+    if (strcmp(topic, _topicSfx) == 0 || strcmp(topic, _topic) == 0) {
       sendMagicPacket();
       return true;
     }
@@ -47,6 +65,7 @@ public:
   }
 
   void onMqttConnect(bool sessionPresent) override {
+    deriveTopicSuffix();
     if (WLED_MQTT_CONNECTED) {
       mqtt->subscribe(_topic, 0);
       DEBUG_PRINTLN(F("WoL: Subscribed to MQTT topic"));
@@ -65,6 +84,7 @@ public:
     if (top["mac"].as<String>().length() > 0)   strlcpy(_mac,   top["mac"],   sizeof(_mac));
     if (top["topic"].as<String>().length() > 0) strlcpy(_topic, top["topic"], sizeof(_topic));
     _macParsed = false;
+    deriveTopicSuffix();
     return true;
   }
 
